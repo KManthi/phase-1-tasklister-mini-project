cb7793daa88b04df2a6a01d5500f5ebb49a34051
Daniel <dani3lkamanthi@gmail.com>
2024-03-28 21:20:19 +0300

    Modifies index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const newTaskForm = document.querySelector('form');
+
+  newTaskForm.addEventListener('submit' , (e) => {
+    
+    e.preventDefault();
+    
+    const newToDo = e.target.querySelector("#new-task-description").value
+
+    buildToDo(newToDo);
+
+    newTaskForm.reset();
+  })
 });
+
+function buildToDo (newToDo) {
+  
+  const taskList = document.createElement('li');
+  
+  const deleteFromList = document.createElement('button');
+
+  deleteFromList.textContent = 'x';
+
+  taskList.textContent = newToDo;
+  
+  taskList.appendChild(deleteFromList);
+
+  document.querySelector('#tasks').appendChild(taskList);
+
+  deleteFromList.addEventListener('click', deleteTask);
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
